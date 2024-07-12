3020ccdfd62c0f6cfbd561117aa0c862423af3d2
Mikhail Kobtsev <136962134+Xatt1408@users.noreply.github.com>
2024-07-12 17:19:12 +0300

    ApiTest.java

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -5,31 +5,33 @@ import org.junit.jupiter.api.*;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-
 public class ApiTest {
-    private final static String RESPONSE = "This is expected to be sent back as part of response body.";
+
+    private static final String BASE_URI = "https://postman-echo.com";
+    private static final String RESPONSE = "This is expected to be sent back as part of response body.";
+
+    private RequestSpecification requestSpecification;
+    private ResponseSpecification responseSpecification;
 
     @BeforeEach
-    public void setURLandOK200() {
-        RequestSpecification requestSpec = new RequestSpecBuilder()
-                .setBaseUri("https://postman-echo.com")
+    public void setUp() {
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
                 .build();
 
-        ResponseSpecification responseSpecOK200 = new ResponseSpecBuilder()
+        responseSpecification = new ResponseSpecBuilder()
                 .expectStatusCode(200)
                 .build();
-
-        requestSpecification = requestSpec;
-        responseSpecification = responseSpecOK200;
     }
 
     @Test
     public void getRequestTest() {
-
         given()
-                .when()
+                .spec(requestSpecification)
+        .when()
                 .get("/get?foo1=bar1&foo2=bar2")
-                .then()
+        .then()
+                .spec(responseSpecification)
                 .log().all()
                 .body("args.foo1", equalTo("bar1"))
                 .body("args.foo2", equalTo("bar2"));
@@ -37,26 +39,28 @@ public class ApiTest {
 
     @Test
     public void postRawTextTest() {
-
         given()
+                .spec(requestSpecification)
                 .body("")
-                .when()
+        .when()
                 .post("/post")
-                .then()
+        .then()
+                .spec(responseSpecification)
                 .log().all()
                 .body("json", equalTo(null));
     }
 
     @Test
     public void postFormDataTest() {
-
         given()
+                .spec(requestSpecification)
                 .contentType("application/x-www-form-urlencoded; charset=utf-8")
                 .formParam("foo1", "bar1")
                 .formParam("foo2", "bar2")
-                .when()
+        .when()
                 .post("/post")
-                .then()
+        .then()
+                .spec(responseSpecification)
                 .log().all()
                 .body("form.foo1", equalTo("bar1"))
                 .body("form.foo2", equalTo("bar2"));
@@ -64,37 +68,40 @@ public class ApiTest {
 
     @Test
     public void putRequestTest() {
-
         given()
+                .spec(requestSpecification)
                 .body(RESPONSE)
-                .when()
+        .when()
                 .put("/put")
-                .then()
+        .then()
+                .spec(responseSpecification)
                 .log().all()
                 .body("data", equalTo(RESPONSE));
     }
 
     @Test
     public void patchRequestTest() {
-
         given()
+                .spec(requestSpecification)
                 .body(RESPONSE)
-                .when()
+        .when()
                 .patch("/patch")
-                .then()
+        .then()
+                .spec(responseSpecification)
                 .log().all()
                 .body("data", equalTo(RESPONSE));
     }
 
     @Test
     public void deleteRequestTest() {
-
         given()
-                .when()
+                .spec(requestSpecification)
+        .when()
                 .delete("/delete")
-                .then()
+        .then()
+                .spec(responseSpecification)
                 .log().all()
                 .body("json", nullValue())
-                .body("url", equalTo("https://postman-echo.com/delete"));
+                .body("url", equalTo(BASE_URI + "/delete"));
     }
 }
